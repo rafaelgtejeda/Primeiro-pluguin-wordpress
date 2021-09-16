ab434445e980f80e5c2456730de5803cf13b29a3
rafael.tejeda <rafael.tejeda@iconnectdobrasil.com.br>
2021-09-16 11:53:34 -0300

    Criado bd votos receitas

--- includes/activate.php
+++ includes/activate.php
@@ -5,4 +5,18 @@ function rt_activate_pluguin() {
         wp_die(__('Você precisa atualizar o WordPress para usar este pluguin.', 'receitas'));
     }
 
+    global $wpdb;
+
+    $sql = "CREATE TABLE ".$wpdb->prefix."receita_votos(
+        ID BIGINT(20) NOT NULL AUTO_INCREMENT, 
+        receita_id BIGINT(20) NOT NULL,
+        voto TINYINT(1) NOT NULL,
+        user_ip VARCHAR(32) NOT NULL,
+        PRIMARY KEY (ID)
+    ) " .$wpdb->get_charset_collate();
+
+    require_once(ABSPATH.'/wp-admin/includes/upgrade.php'); 
+    
+    dbDelta( $sql );
+
 }
